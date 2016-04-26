226272a2696232fbebd6adffe55128436d5ad724
Nick Morrison <nmorrisn@bu.edu>
2016-04-26 02:24:13 +0000

    Fixed syntax error in print

--- Modules/glucObject.py
+++ Modules/glucObject.py
@@ -11,19 +11,18 @@ class glucObject:
         try:
             proc = subprocess.call('sudo /home/pi/bluepyenv/eHealth_raspberrypi_v2.4/glucode',shell=True)
         except OSError as msg:
-            print "OSError({0}): {1}".format(msg.errno, msg.strerror)
-            continue
+            print ("OSError({0}): {1}".format(msg.errno, msg.strerror))
         except:
-            print "Unexpected error:", sys.exc_info()[0]
+            print ("Unexpected error:", sys.exc_info()[0])
             raise
         
         try:
             f = open(completeName, "r")
             glucResults = f.readline()
         except IOError as e:
-            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            print ("I/O error({0}): {1}".format(e.errno, e.strerror))
         except:
-            print "Unexpected error:", sys.exc_info()[0]
+            print ("Unexpected error:", sys.exc_info()[0])
             raise
         else:
             f.close()
